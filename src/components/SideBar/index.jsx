9d337344e32e86da013e772034f29b8b9baf5b23
Antoni Hidalgo <antoni.hidalgo.3@gmail.com>
2023-10-16 06:13:45 -0400

    More Responsive side bar

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -8,10 +8,12 @@ import { REPORTS_ROUTE} from '../../consts/Routes'
 import { logout } from '../../helpers/logout'
 import { AdminMenus, NormalUserMenu } from '../../consts/Menus'
 import { useCheckRole } from '../../hooks/useCheckRole'
+import useScreenSize from '../../hooks/useScreenSize'
 
 const SideBar = ({ children }) => {
   const location = useLocation();
   const [isActive, setIsActive] = useState(window.location.pathname.startsWith(REPORTS_ROUTE));
+  const {width, maxHeight} = useScreenSize() 
   let menus = [];
   useEffect(() => {
     setIsActive(location.pathname.startsWith('/dashboard/'+REPORTS_ROUTE));
@@ -23,6 +25,107 @@ const SideBar = ({ children }) => {
   else{
     menus = NormalUserMenu;
   }
+  return width > 1440 
+    ?<SideBarBig menus={menus} setSession={setSession} isActive={isActive}>{children}</SideBarBig>
+    :<SideBarSmall menus={menus} setSession={setSession} isActive={isActive}>{children}</SideBarSmall>
+}
+const SideBarSmall = ({menus, setSession, isActive, children}) =>{return (
+  <div className='container-fluid AppContainer'>
+    <div className='row'>
+      <div className='col-3 SideBarCol d-flex flex-column flex-shrink-0 p-3'>
+        <div className='d-flex pb-4 pt-3 align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none'>
+          <img
+            src='/gedecu.png'
+            className='img-fluid bi pe-none me-2'
+          />
+        </div>
+        <ul className='nav nav-pills flex-column mb-auto'>
+          {menus.map((Menu, index) => (
+            <li
+              key={index}
+              className='SideBarItem p-1'
+            >
+              {
+                !Menu.others
+                  ? <NavLink
+                      className='nav-link'
+                      to={Menu.link}
+                    >
+                    <ReactSVG
+                      className='bi me-2 '
+                      wrapper='span'
+                      src={`/${Menu.src}.svg`}
+                    />
+                    {Menu.title}
+                  </NavLink>
+                  : <Accordion>
+                    <Accordion.Item className={isActive ? "active" : ""}>
+                      <Accordion.Header>
+                        <NavLink
+
+                          className='nav-link'
+                          to={REPORTS_ROUTE}
+                        >
+                          <ReactSVG
+                            className='bi me-2 '
+                            wrapper='span'
+                            src={`/${Menu.src}.svg`}
+                          />
+                          {Menu.title}
+                        </NavLink>
+                      </Accordion.Header>
+                      <Accordion.Body>
+                        {
+                        Menu.others.map((e, index) =>
+                          <NavLink
+                            className='nav-link'
+                            to={e.link}
+                            key={index}
+                          >
+                            {e.name}
+                          </NavLink>
+                        )
+                      }
+                      </Accordion.Body>
+                    </Accordion.Item>
+                  </Accordion>
+              }
+            </li>
+          ))}
+          <li className='SideBarItem p-1' style={{opacity: 0.5, textWrap:'nowrap'}}>
+            <a href='#' className='nav-link'>
+              <ReactSVG 
+                className='bi me-2 '
+                wrapper='span'
+                src={`/world.svg`}
+              />
+                Países (próximamente)
+            </a>
+          </li>
+        </ul>
+        <div>
+          <div className='pb-5 nav nav-pills flex-column mb-auto'>
+            <Link className='nav-link' onClick={() => logout().then().finally(() => setSession(false))}>
+              <ReactSVG
+                className='bi me-2 '
+                wrapper='span'
+                src='/sign-in.svg'
+              />
+              Cerrar Sesión
+            </Link>
+          </div>
+          <img src='/logo-broders.png' alt='Logo broders' />
+        </div>
+      </div>
+      <div className='col-9' style={{ maxHeight: '100vh', overflowY: 'scroll' }}>
+        {children}
+      </div>
+    </div>
+  </div>
+  )
+}
+const SideBarBig = ({menus, setSession, isActive, children})=>{
+  
   return (
     <div className='container-fluid AppContainer'>
       <div className='row'>
@@ -118,5 +221,4 @@ const SideBar = ({ children }) => {
     </div>
   )
 }
-
 export default SideBar
