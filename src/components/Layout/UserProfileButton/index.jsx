3b2570fdea195a1522d3b5a87860ed37257d56c3
Antonio Sotillo <antoniosotillo14@hotmail.com>
2024-02-26 20:21:13 -0400

    Refact: add User Profile Avatar

--- src/components/Layout/UserProfileButton/index.jsx
+++ src/components/Layout/UserProfileButton/index.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { SessionContext } from "../../../context/SessionContext";
 import "./UserProfileButton.css";
 import { useNavigate } from "react-router-dom";
-import { DASHBOARD_ROUTE, USERS_ROUTE } from "../../../consts/Routes";
+import { USERS_ROUTE } from "../../../consts/Routes";
 
 export default function UserProfileButton() {
     const { session } = useContext(SessionContext);
@@ -12,9 +12,18 @@ export default function UserProfileButton() {
         navigate(`${USERS_ROUTE}/profile`);
     }
 
+    const words = session.name.split(" ").filter((name) => name);
+    let firstLetters = words.at(0)[0];
+    firstLetters += words.pop()[0];
+
     return (
         <div onClick={goToProfile} className="d-flex justify-content-end align-items-center UserProfileButton">
-            <img src="" alt="Avatar" width={32} height={32} className="me-3 rounded" />
+            {/* <img src="" alt="Avatar" width={32} height={32} className="me-3 rounded" /> */}
+            <div className="UserProfileAvatar">
+                {
+                    firstLetters.toUpperCase()
+                }
+            </div>
             <div>
                 <p className="m-0 fw-semibold UserName">
                     {
